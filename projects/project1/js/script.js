5f738c2498d26427b65f94128d46817964f1ac73
Harout77 <harout@live.ca>
2019-02-07 14:46:50 -0500

    create a slider with limits

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,3 +9,31 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+
+$( function() {
+   var handle = $( "#custom-handle" );
+   $( "#slider" ).slider({
+     create: function() {
+       handle.text( $( this ).slider( "value" ) );
+     },
+     slide: function( event, ui ) {
+       handle.text( ui.value );
+
+       if(ui.value > 80){
+
+         $(this).slider( "disable" );
+       }
+       else {
+
+         $(this).slider( "enable" );
+
+       }
+     }
+
+
+   });
+ } );
+
+
+ $('body').css('cursor', 'none');
