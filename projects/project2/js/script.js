b96c5e7f3eefc286b6eee5bf6a369f491157a365
Harout77 <harout@live.ca>
2019-03-14 20:40:57 -0400

    P2 Working question on click
    a voice asks the question on click
    next the options

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,7 +1,45 @@
-$(document).ready(function () {
 
-  // Make sure we can run annyang (Chrome-dependent), otherwise no point
-  if (annyang) {
-    $(document).on('click',start);
-  }
+
+
+let voice = 'US English Male';
+
+
+
+
+let voiceParameters = {
+  pitch: 1,
+  rate: 1,
+  volume: 2
+}
+
+
+// $(document).ready(function () {
+//
+//   // Make sure we can run annyang (Chrome-dependent), otherwise no point
+//   // if (annyang) {
+//   //   $(document).on('click',start);
+//   // }
+//
+//
+// //
+// // responsiveVoice.speak("hello world", "UK English Male", {onstart: StartCallback, onend: EndCallback});
+//
+//
+// });
+
+
+$(document).ready(function() {
+
+
+$(document).on('click',function () {
+
+// question sentence goes here
+   question("HELLO WORLD");
+
+ });
 });
+
+//// function for the question
+function question (text) {
+  responsiveVoice.speak(text,voice,voiceParameters);
+}
