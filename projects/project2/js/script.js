0e21568af78279d269028ca00c827b195e84a7b2
Harout77 <harout@live.ca>
2019-03-14 20:27:33 -0400

    P2 Button to speak
    a button that speaks a sentence

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,50 +1,7 @@
-"use strict";
+$(document).ready(function () {
 
-/*****************
-
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
-******************/
- // Possible answer array
-
-// let answers =[
-//   "one",
-//   "two",
-//   "three",
-//   "four",
-//   "five",
-//   "six",
-//   "seven",
-//   "eight",
-//   "nine",
-//   "ten"
-// ]
-//
-//
-//
-// let currentAnswer ='';
-
-$(document).ready(function() {
-
+  // Make sure we can run annyang (Chrome-dependent), otherwise no point
   if (annyang) {
-    // Let's define our first command. First the text we expect, and then the function it should call
-    var commands = {
-      'say hello (to my little) friend': greeting
-
-    };
-
-    // Add our commands to annyang
-    annyang.addCommands(commands);
-
-    // Start listening. You can call this here, or attach this call to an event, button, etc.
-    annyang.start();
+    $(document).on('click',start);
   }
-
-  var greeting = function() {
-    $('#greeting').text('Hello!');
-  }
-}
+});
