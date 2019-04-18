195ab0214a74b98f510e5f452184ee7a00ecdb95
Harout77 <harout@live.ca>
2019-04-18 10:27:11 -0400

    Bckground video with a button

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -9,3 +9,29 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+	if (vid.paused) {
+vid.play();
+		pauseButton.innerHTML = "Pause";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+	}
+})
